ab97688af85493015b9598fb7cd89b989039deb5
Satish Prasad <spd61185@gmail.com>
2022-03-31 05:58:46 +0530

    Dummy Project

---
+++ springboot-junit/src/test/java/com/satishlabs/SpringbootJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satishlabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
